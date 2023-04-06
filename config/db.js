6939031665c991dd0dae4e60c57ccb68a2c8dea6
Usama Qazi <usamyharis@yahoo.com>
2023-04-07 00:29:35 +0500

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+module.exports = {
+    url: "mongodb+srv://usamyismy7:<password>@cluster0.ljbuz8r.mongodb.net/?retryWrites=true&w=majority"
+}
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://usamyismy7:<password>@cluster0.ljbuz8r.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
